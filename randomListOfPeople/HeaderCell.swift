09e9ae68290d8277cedbf8df00328012e12dfe91
Tanya <taaapv@gmail.com>
2022-03-10 13:32:25 +0400

    Add new functional

---
+++ randomListOfPeople/HeaderCell.swift
@@ -0,0 +1,14 @@
+//
+//  HeaderCell.swift
+//  randomListOfPeople
+//
+//  Created by Татьяна on 09.03.2022.
+//
+
+import UIKit
+
+class HeaderCell: UITableViewCell {
+    
+    @IBOutlet var personLabel: UILabel!
+    
+}
--- randomListOfPeople/SectionsTableViewController.swift
+++ randomListOfPeople/SectionsTableViewController.swift
@@ -21,17 +21,57 @@ class SectionsTableViewController: UITableViewController {
         return persons.count
     }
     
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        let person = persons[section]
-        return person.name + " " + person.surname
-    }
+    // 1 variant
+//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        let person = persons[section]
+//        return person.name + " " + person.surname
+//    }
 
+    // 2 variant
+//    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        let headerCell = tableView.dequeueReusableCell(withIdentifier: "HeaderCell") as! HeaderCell
+//
+//        let person = persons[section]
+//        headerCell.personLabel.text = person.name + " " + person.surname
+//
+//        return headerCell
+//    }
+    
+    // 3 variant
+//    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        let person = persons[section]
+//
+//        let label = UILabel()
+//        label.backgroundColor = .gray
+//        label.textColor = .white
+//        label.textAlignment = .center
+//        label.font = UIFont.boldSystemFont(ofSize: 24)
+//        label.text = person.name + " " + person.surname
+//
+//        return label
+//    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let person = persons[section]
+        
+        let myView = UIView()
+        myView.backgroundColor = .gray
+        
+        let label = UILabel(frame: CGRect(x: 20, y: 3, width: 300, height: 20))
+        label.text = person.name + " " + person.surname
+        label.textColor = .white
+        
+        myView.addSubview(label)
+        
+        return myView
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let person = persons[indexPath.section]
         
         var content = cell.defaultContentConfiguration()
@@ -47,49 +87,5 @@ class SectionsTableViewController: UITableViewController {
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
